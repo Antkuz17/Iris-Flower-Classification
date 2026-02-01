03adc7aa355bb57e595aaea091cafdd060fc345c
Anton Kuzmichev <Antonkuzmichev17@gmail.com>
2026-01-31 23:24:45 -0500

    Normalization + and data setup for the network training is configured

--- src/dataExtract.cpp
+++ src/dataExtract.cpp
@@ -3,20 +3,13 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include "headers.hpp"
 
 
-
-// The struct used to store each data point from the csv file
-struct Record {
-    double pedal_length;
-    double pedal_width;
-    double sepal_length;
-    double sepal_width;
-    std::string flower_type;
-};
-
-
-int main () {
+std::vector<std::vector<Record>> getCsvData () {
     std::ifstream file("../data/iris.data");
 
     // An array of all the the data point objects
@@ -44,19 +37,79 @@ int main () {
 
         
         
-        listDataPoints[currentLine].pedal_length = std::stod(tokens[0]);
-        listDataPoints[currentLine].pedal_width = std::stod(tokens[1]);
-        listDataPoints[currentLine].sepal_length = std::stod(tokens[2]);
-        listDataPoints[currentLine].sepal_width = std::stod(tokens[3]);
+    listDataPoints[currentLine].sepal_length = normalize_sepal_length(std::stod(tokens[0])); 
+    listDataPoints[currentLine].sepal_width = normalize_sepal_width(std::stod(tokens[1]));  
+    listDataPoints[currentLine].pedal_length = normalize_pedal_length(std::stod(tokens[2])); 
+    listDataPoints[currentLine].pedal_width = normalize_pedal_width(std::stod(tokens[3]));  
+    listDataPoints[currentLine].flower_type = tokens[4];     
 
-        
-        currentLine++;
+    currentLine++;
         
     }
-    std::cout << currentLine;
 
     file.close();
+
+    // The default iris dataset is ordered, so we need to shuffle it before splitting it into training and testing data
+    shuffleVector(listDataPoints, 150);
+
+    std::vector<std::vector<Record>> result = splitData(120, 30, listDataPoints);
+
+    // Free the heap memory
+    delete[] listDataPoints;
+
+    // Return the vector containing the training and testing data
+    return result;
+}
+
+// Returns a vector containing two vectors, the first being the training data and the second being the testing data
+std::vector<std::vector<Record>> splitData(int trainNum, int testNum, Record* dataPoints) {
+
+    // The vector is going to hold the training data vector at index 0 and the testing data vector at index 1
+    std::vector<std::vector<Record>> array(2);
+
+    array[0] = std::vector<Record>(trainNum);
+    for(int i{}; i < trainNum; i++) {
+        array[0][i] = dataPoints[i];
+    }
+
+    array[1] = std::vector<Record>(testNum);
+    for(int i{}; i < testNum; i++) {
+        array[1][i] = dataPoints[i + trainNum];
+    }
+
+    return array;
+}   
+
+
+void shuffleVector(Record * dataPoints, int size) {
+
+    // 42 for the memes and the funnies 
+    std::mt19937 seedValue(42);
+
+    // Shuffle the vector
+    std::shuffle(dataPoints, dataPoints + size, seedValue);
 }
 
 
+double normalize_pedal_length(double value){
+    return ((value - 1.0) / 5.9);
+}
+
+double normalize_pedal_width(double value){
+    return ((value - 0.1) / 2.4);
+}
+
+double normalize_sepal_length(double value){
+    return ((value - 4.3) / 3.6);
+}
+
+double normalize_sepal_width(double value){
+    return ((value - 2.0) / 2.4);
+}
+
 // g++ dataExtract.cpp -o testing ; ./testing
+
+
+int main(){
+    std::cout << "Testing data extraction" << std::endl;
+}
---
+++ src/headers.hpp
@@ -0,0 +1,24 @@
+#ifndef DATA_EXTRACT_HPP
+#define DATA_EXTRACT_HPP
+
+#include <vector>
+#include <string>
+
+struct Record {
+    double pedal_length;
+    double pedal_width;
+    double sepal_length;
+    double sepal_width;
+    std::string flower_type;
+};
+
+std::vector<std::vector<Record>> getCsvData();
+std::vector<std::vector<Record>> splitData(int trainNum, int testNum, Record* dataPoints);
+void shuffleVector(Record* dataPoints, int size);
+
+double normalize_pedal_length(double value);
+double normalize_pedal_width(double value);
+double normalize_sepal_length(double value);
+double normalize_sepal_width(double value);
+
+#endif
