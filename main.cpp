cd58261aa97d8e5d64beea1a0cdf09159853d6a0
Anton Kuzmichev <Antonkuzmichev17@gmail.com>
2026-02-15 20:32:55 -0500

    cmake files created

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 // Gonna recode the entire network in c++ 
 // Why? cause i love to suffer and feel pain
+#include <iostream>
 
 int main() {
+    std::cout << "Hopefully this works" << std::endl;
     return 0;
 }
 
